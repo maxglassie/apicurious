4147649525c784ecea8aa05255599d08c49f0f01
Max Glassie <msglassie@gmail.com>
2017-03-21 13:59:49 -0600

    commit adds oauth_response and data

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,15 @@
 class SessionsController < ApplicationController
     def create
-      binding.pry
       code = params['code']
       client_id = ENV['client_id']
       client_secret = ENV['client_secret']
 
       response  = Faraday.post("https://github.com/login/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&code=#{code}")
+      access_token = response.body.split(/\W+/)[1]
+
+      oauth_response = Faraday.get("https://api.github.com/user?access_token=#{access_token}")
+      data = JSON.parse(oauth_response.body)
+
+      
     end
 end
