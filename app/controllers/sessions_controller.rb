07136b456a63f41327e30a5b1c1bbcdef4df8b44
Max Glassie <msglassie@gmail.com>
2017-03-23 17:56:20 -0600

    removes pry

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
       access_token = github_oauth.access_token
       data = github_oauth.data
 
-      binding.pry
       user = User.from_github(data, access_token)
       session[:user_id] = user.id
 
